e6a0a1dcc76cf6f759040545bd1582951b44d46f
Kyle Mera <kmera1164@yahoo.com>
2015-06-10 23:56:00 -0400

    Added links between pages
    Added links between pages are error messages / navigation to pages when
    a request is made

--- scripts/controllers/user.js
+++ scripts/controllers/user.js
@@ -12,11 +12,23 @@ angular.module('completeConceptStrength.userCtrl', [])
 
   	$scope.register = function(user) {
 
-  		var promise = userService.register(user);
+  	  var promise = userService.register(user);
       promise.then(function(res)
       {
-        $scope.success = res;
-      })
+		if(res == "true") {
+			console.log("Registration successful");
+			$scope.error = "";
+			location.href = "login.html";
+		} else {
+			console.log("Error registering user");
+			$scope.error = "Error registering user";	
+		}
+        
+      }, function(error) {
+		  	console.log("Error registering user");
+			console.log("Response: " + error);
+			$scope.error = "Error registering user";
+	  })
   	}
 
     $scope.authenticate = function(user) {
@@ -24,12 +36,24 @@ angular.module('completeConceptStrength.userCtrl', [])
       var promise = userService.authenticate(user);
       promise.then(function(res)
       {
-        $scope.user = res;
-        $scope.code = 200;
-      }, function(status) {
-          $scope.user = {};
-          $scope.code = status;
+			console.log("Login successful");
+        	$scope.error = "";
+			
+			if(res.userType == "ATHLETE") {
+				location.href = "homepage_athlete.html"	
+			} else if(res.userType == "COACH") {
+				location.href = "homepage_trainer.html"
+			}
+      }, function(error) {
+            console.log("Error logging in user");
+			console.log("Response: " + error);
+			$scope.error = "Username or password is incorrect";
       })
     }
+	
+	$scope.logout = function() {
+		console.log("Logging out");
+		location.href = "login.html";
+	}
 
   });
